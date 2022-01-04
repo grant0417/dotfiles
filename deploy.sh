05a7889bc3f288cf19aebcee54748ea7ecb85ccf
Grant G <grant0417@users.noreply.github.com>
2022-01-04 14:39:45 -0800

    Fix shebang

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 
 DEPLOY_DIR=$(dirname $0)/
 rsync --exclude ".git/" --exclude "deploy.sh"  --exclude "screenshots" --exclude "README.md" --exclude "pkglist.txt" --exclude "aurlist.txt" --exclude "install_deploy.sh" -avhE --no-perms $DEPLOY_DIR $HOME
